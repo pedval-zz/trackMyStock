a82413fd0cfc522146164e994dc5079562306c1e
unknown <valenciap@ECE-1017.TC-ONLINE.local>
2014-10-16 15:59:37 +0100

    Create MainActivity and test to move from CoverActivity to MainActivity

--- app/src/main/java/com/pedrovalencia/trackmystock/activities/CoverActivity.java
+++ app/src/main/java/com/pedrovalencia/trackmystock/activities/CoverActivity.java
@@ -2,13 +2,17 @@ package com.pedrovalencia.trackmystock.activities;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.pedrovalencia.trackmystock.R;
 import com.pedrovalencia.trackmystock.util.SystemUiHider;
+
+import java.io.IOException;
 
 
 /**
@@ -19,6 +23,9 @@ import com.pedrovalencia.trackmystock.util.SystemUiHider;
  */
 public class CoverActivity extends Activity {
 
+    public static final int DELAY = 3000;
+    private static final String LOG_TAG = CoverActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +33,10 @@ public class CoverActivity extends Activity {
         //Block that hides the status bar
         if(Build.VERSION.SDK_INT < 16) {
             //For version lower than Jellybean
-
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         } else {
             //For version greater or equal than Jellybean
-
             View decorView = getWindow().getDecorView();
             // Hide the status bar.
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -41,10 +45,22 @@ public class CoverActivity extends Activity {
             ActionBar actionBar = getActionBar();
             actionBar.hide();
         }
-
         setContentView(R.layout.activity_cover);
-
     }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            Thread.sleep(DELAY);
+        } catch (InterruptedException ex) {
+            Log.e(LOG_TAG, "Error while waiting for the delay "+DELAY +" milliseconds", ex);
+        } finally{
+            //Start new activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            //Destroy the current activity. We don't want it in the stack
+            //this.finish();
+        }
+    }
 }
--- app/src/test/java/com/pedrovalencia/trackmystock/activities/CoverActivityTest.java
+++ app/src/test/java/com/pedrovalencia/trackmystock/activities/CoverActivityTest.java
@@ -1,6 +1,7 @@
 package com.pedrovalencia.trackmystock.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.widget.TextView;
 
 import com.pedrovalencia.trackmystock.R;
@@ -12,6 +13,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.util.ActivityController;
 
 import static org.junit.Assert.assertTrue;
 
@@ -22,31 +25,33 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricTestRunner.class)
 public class CoverActivityTest  {
 
-    private Activity activity;
-
-    @Before
-    public void setUp() throws Exception {
-        activity = Robolectric.buildActivity(CoverActivity.class).create().get();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        activity.finish();
-    }
-
     @Test
     public void testActivityNotNull() throws Exception {
+        Activity activity = Robolectric.buildActivity(CoverActivity.class).create().get();
         assertTrue("Activity is null", activity != null);
     }
 
     @Test
     public void testTextInActivity() throws Exception {
+        Activity activity = Robolectric.buildActivity(CoverActivity.class).create().get();
         TextView coverTextView = (TextView) activity.findViewById(R.id.fullscreen_content);
         assertTrue("Wrong cover text",
                 coverTextView.getText().equals(activity.getString(R.string.main_title_text)));
         assertTrue("Wrong cover text color",
                 coverTextView.getCurrentTextColor() == activity.getResources().getColor(R.color.main_text_color));
 
+    }
+
+    @Test
+    public void goToNextActivity() throws Exception {
+        ActivityController activityController = Robolectric.buildActivity(CoverActivity.class).create().start().resume();
+
+        Activity activity = (Activity)activityController.get();
+        assertTrue("Activity is null", activity != null);
+
+        Intent intent = Robolectric.shadowOf(activity).peekNextStartedActivity();
+        assertTrue("Type of activity is not MainActivity class: "+intent.getComponent().getClassName(),
+                intent.getComponent().getClassName().equals(MainActivity.class.getCanonicalName()));
     }
 
 
