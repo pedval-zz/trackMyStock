7041261f719611f310c7479c33224fa2d65a48cf
Pedro <pedro.valencia@thomascookonline.com>
2015-01-09 17:18:47 +0000

    Fixing and creating new tests

--- app/src/test/java/com/pedrovalencia/trackmystock/fragments/DetailFragmentTest.java
+++ app/src/test/java/com/pedrovalencia/trackmystock/fragments/DetailFragmentTest.java
@@ -1,9 +1,13 @@
 package com.pedrovalencia.trackmystock.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuInflater;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +23,9 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowAlertDialog;
+import org.robolectric.tester.android.view.TestMenu;
+import org.robolectric.tester.android.view.TestMenuItem;
 import org.robolectric.util.ActivityController;
 import org.robolectric.util.FragmentTestUtil;
 
@@ -116,6 +123,39 @@ public class DetailFragmentTest {
         TextView lowTextView = (TextView)detailFragment1.getView().findViewById(R.id.detail_fragment_low_content);
         assertNotNull("Low price field is null", lowTextView);
         assertTrue("Low price is not the correct one: "+lowTextView.getText().toString(), "30.12".equals(lowTextView.getText().toString()));
+
+    }
+
+    @Test
+    @Config(qualifiers = "v10")
+    public void deleteCompanyTest() throws Exception {
+        ActivityController<DetailActivity> activityController =
+                Robolectric.buildActivity(DetailActivity.class).create();
+        Activity activity = activityController.get();
+
+       detailFragment.onAttach(activity);
+
+        //Simulate a Menu object
+        TestMenu testMenu = new TestMenu(Robolectric.application);
+        new MenuInflater(Robolectric.application).inflate(R.menu.detail_fragment, testMenu);
+
+        assertTrue("There is not just one item in the menu: " + testMenu.size(), testMenu.size() == 2);
+
+        //Select item 1 (delete company)
+        TestMenuItem menuItem = (TestMenuItem)testMenu.getItem(0);
+        FragmentActivity detailActivity = detailFragment.getActivity();
+        detailActivity.onOptionsItemSelected(menuItem);
+        menuItem.click();
+
+        assertTrue("First menu is not delete company: " + menuItem.getItemId(),
+                menuItem.getItemId() == R.id.action_discard_company);
+
+        AlertDialog alert =  ShadowAlertDialog.getLatestAlertDialog();
+        /*assertNotNull("alert is null", alert);
+        alert.getButton(DialogInterface.BUTTON_POSITIVE).performClick();
+        ShadowAlertDialog sAlert = Robolectric.shadowOf(alert);
+        assertTrue("Alert title does not match: " + sAlert.getTitle().toString(),
+                detailActivity.getString(R.string.dialog_title).equals(sAlert.getTitle().toString()));*/
 
     }
 
